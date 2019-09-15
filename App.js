cca3624b59e3b3aa72151b006fd8ee93d6026453
Aleksandr Klimenkov <prisioner@mail.ru>
2019-09-15 15:40:37 +0300

    server rendering

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Catalog from '~/src/components/Catalog'
+
+export default class App extends Component {
+  render() {
+    return(
+      <div>
+        <Catalog />
+      </div>
+    )
+  }
+}
---
+++ render.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from "./App";
+
+const render = () => {
+  return ReactDOMServer.renderToString(<App />)
+}
+
+export default render;
